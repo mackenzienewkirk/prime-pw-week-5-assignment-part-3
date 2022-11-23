78965392badc7f52b6e60e523ed77baaf4f3301a
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-11-22 20:33:18 -0600

    Created albums as objects and showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,52 @@ console.log('***** Music Collection *****')
 
 let collection = {};
 
+let albumOne = {
+    title: 'Love And Hate',
+    artist: 'Michael Kiwanuka',
+    yearPublished: 2016
+}
+let albumTwo = {
+    title: 'Coloring Book',
+    artist: 'Chance The Rapper',
+    yearPublished: 2016
+}
+let albumThree = {
+    title: 'Joanne',
+    artist: 'Lady Gaga',
+    yearPublished: 2016
+}
+let albumFour = {
+    title: 'Renaissance',
+    artist: 'Beyonce',
+    yearPublished: 2022
+}
+let albumFive = {
+    title: 'I Just Dropped By To Say Hello',
+    artist: 'Johnny Hartman',
+    yearPublished: 1963
+}
+let albumSix = {
+    title: 'Blood On The Tracks',
+    artist: 'Bob Dylan',
+    yearPublished: 1975
+}
+
+console.log(albumOne);
+console.log(albumTwo);
+console.log(albumThree);
+console.log(albumFour);
+console.log(albumFive);
+console.log(albumSix);
+
+
 function addToCollection(title, artist, yearPublished){
-    console.log();
+    collection.push(albumOne);
+return albumOne;
+}
+
+console.log(collection);
+
+function showCollection(){
+
 }
